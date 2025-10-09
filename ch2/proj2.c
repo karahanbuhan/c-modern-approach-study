2a39c0e63aa4237ef6d980396b3251adc65cf397
Karahan Buhan <contact@karahanbuhan.com>
2025-10-09 19:58:27 +0300

    Add project 2

---
+++ ch2/proj2.c
@@ -0,0 +1,16 @@
+/* Write a program that computes the volume of a sphere with a 10-meter radius, using the for-
+mula v = 4/3πr3. Write the fraction 4/3 as 4.0f/3.0f. (Try writing it as 4/3. What hap-
+pens?) */
+
+#include <stdio.h>
+
+#define PI 3.1415926535f
+
+int main(void) {
+    float radius = 10.0f;
+    float volume = radius * radius * radius * PI * 4.0f/3.0f;
+    /* float volume = radius * radius * radius * PI * 4/3; The program worked as expected even as 4/3 because radius is float thus 4 is promoted to 4.0f */
+    /* float volume = 4/3 * radius * radius * radius * PI; However this breaks the program as 4/3 is now 1 as it started directly from integer operation */
+    printf("%.4fm3\n", volume);
+    return 0;
+}
