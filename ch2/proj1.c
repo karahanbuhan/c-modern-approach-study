815921cda2a3dbb37013296ab031500ca5f805f9
Karahan Buhan <contact@karahanbuhan.com>
2025-10-09 19:46:25 +0300

    Add project 1

---
+++ ch2/proj1.c
@@ -0,0 +1,16 @@
+/*
+ Write a program that uses printf to display the following picture on the screen:
+         *
+        *
+       *
+  *   *
+   * *
+    *
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
+    return 0;
+}
