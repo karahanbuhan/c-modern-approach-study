59b85cf5b2f9ccf37d8cd686e33df7de1fcd6091
Karahan Buhan <contact@karahanbuhan.com>
2025-10-09 12:49:12 +0300

    Rename fahrenheit to celsius

--- ch2/test.c
+++ ch2/test.c
@@ -4,6 +4,7 @@
 #define RECIPROCAL_OF_PI (1.0f / PI)
 
 int main() {
-    printf("Reciprocal of PI is: %.5f\n", RECIPROCAL_OF_PI);
+    printf("Reciprocal of PI is: %.5f\n",
+         RECIPROCAL_OF_PI);
     return 0;
 }
