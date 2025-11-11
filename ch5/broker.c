a580bc26809ae3eec5e957f90f73f5ce33306528
Karahan Buhan <contact@karahanbuhan.com>
2025-11-11 10:18:37 +0300

    Add chapter5/broker

---
+++ ch5/broker.c
@@ -0,0 +1,29 @@
+/* Calculates a broker's commission */
+
+#include <stdio.h>
+
+int main(void) {
+    float amount = 0.00f, comission = 0.00f;
+
+    printf("Enter value of trade: ");
+    scanf("%f", &amount);
+
+    if (amount < 39) {
+        printf("The minimum charge is $39!");
+        return 0;
+    } else if (amount < 2500.00f) {
+        comission = 30.0f + amount * (1.70f/100.00f);
+    } else if (amount < 6250.00f) {
+        comission = 56 + amount * (0.66f/100.00f);
+    } else if (amount < 20000.00f) {
+        comission = 76 + amount * (0.34f/100.00f);
+    } else if (amount < 50000.00f) {
+        comission = 100 + amount * (0.22f/100.00f);
+    } else {
+        comission = 255 + amount * (0.09f/100.00f);
+    }
+    
+    printf("Comission: %.2f\n", comission);
+
+    return 0;
+}
