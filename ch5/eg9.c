ef7f3f83cbdb253d0cad2955c4fca74a6f71c006
Karahan Buhan <contact@karahanbuhan.com>
2025-11-15 13:20:17 +0300

    Solve exercise 9

---
+++ ch5/eg9.c
@@ -0,0 +1,50 @@
+/* Are the following if statements equivalent? If not, why not?
+
+if (score >= 90)            if (score < 60)
+    printf("A");                printf("F");
+else if (score >= 80)       else if (score < 70)
+    printf("B");                printf("D");
+else if (score >= 70)       else if (score < 80)
+    printf("C");                printf("C");
+else if (score >= 60)       else if (score < 90)
+    printf("D");                printf("B");
+else                        else
+    printf("F");                printf("A");
+
+*/
+
+/* They are equivalent. Right one is shorter but left one is more intuitive and easier to read. */
+
+#include <stdio.h>
+
+int main(void) {
+    int score = 45;
+
+    if (score >= 90) {
+        printf("A");
+    } else if (score >= 80) {
+        printf("B");
+    } else if (score >= 70) {
+        printf("C");
+    } else if (score >= 60) {
+        printf("D");
+    } else {
+        printf("F");
+    }
+
+    if (score < 60) {
+        printf("F");
+    } else if (score < 70) {
+        printf("D");
+    } else if (score < 80) {
+        printf("C");
+    } else if (score < 90) {
+        printf("B");
+    } else {
+        printf("A");
+    }
+
+    printf("\n");
+
+    return 0;
+}
