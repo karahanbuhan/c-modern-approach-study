ac65d8127c58fc208c5d83b933de881b7f6474d5
Karahan Buhan <contact@karahanbuhan.com>
2025-11-19 18:59:41 +0300

    Add square examples for chapter6

---
+++ ch6/square2.c
@@ -0,0 +1,28 @@
+/* Prints a table of squares using a for statement */
+
+/* Let’s write a program that prints a table of squares. The program will first
+prompt the user to enter a number n. It will then print n lines of output, with
+each line containing a number between 1 and n together with its square:
+This program prints a table of squares.
+Enter number of entries in table: 5
+1 1
+2 4
+3 9
+4 16
+5 25 */
+
+#include <stdio.h>
+
+int main(void) {
+    int i, n;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    for (i = 0; i <= n; i++) {
+        printf("%10d%10d\n", i, i * i);
+    }
+
+    return 0;
+}
